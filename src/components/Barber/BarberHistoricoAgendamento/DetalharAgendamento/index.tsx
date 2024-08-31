9d3549b2e48035acec271c412253b81a85232387
Emanuel Reino <emanuelreino13@gmail.com>
2024-08-31 17:47:05 -0300

    feat: barberInfo integrated with historico agendamento

---
+++ src/components/Barber/BarberHistoricoAgendamento/DetalharAgendamento/index.tsx
@@ -0,0 +1,11 @@
+import styles from "./DetalharAgendamento.module.scss"
+
+const index = () => {
+  return (
+    <div>
+        
+    </div>
+  )
+}
+
+export default index
--- src/components/Barber/BarberInfo/BarberInfo.tsx
+++ src/components/Barber/BarberInfo/BarberInfo.tsx
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { Form, Formik } from "formik";
 import styles from "./BarberInfo.module.scss";
-import { APP_ROUTES } from "@/constants/app-routes";
 import HeaderDetalhamento from "@/components/Header/HeaderDetalhamento";
 import { useRouter } from "next/navigation";
 import { useMutation } from "react-query";
@@ -52,6 +51,9 @@ const BarberInfo: React.FC<BarberInfoProps> = ({
 
   const { mutate } = useMutation(
     async (values: Barbeiro) => {
+      if (!values.idBarber) {
+        throw new Error("ID do barbeiro não está definido.");
+      }
       return putBarbeiroById(values.idBarber, values);
     },
     {
@@ -76,239 +78,247 @@ const BarberInfo: React.FC<BarberInfoProps> = ({
         diretorioAtual={barbeiro.name}
         hrefAnterior={backDetalhamento}
       />
+
       <div className={styles.container}>
         <Formik
-          initialValues={{ ...barberData, address }}
+          initialValues={{
+            ...barberData,
+            address: address
+              ? address
+              : {
+                  /* valores padrão para Address */
+                },
+          }}
           enableReinitialize
           onSubmit={(values, { setSubmitting }) => {
-            mutate({ ...values, address: Number(address?.idAddress) });
+            // Garantir que idBarber seja uma string não opcional
+            const updatedValues: Barbeiro = {
+              idBarber: values.idBarber ?? "", // Usa string vazia se undefined
+              name: values.name ?? "",
+              email: values.email ?? "",
+              cpf: values.cpf ?? "",
+              salary: values.salary ?? 0,
+              admissionDate: values.admissionDate ?? "",
+              workload: values.workload ?? 0,
+              services: values.services ?? [],
+              address: address ? Number(address.idAddress) : 0,
+            };
+
+            mutate(updatedValues);
             setSubmitting(false);
           }}
         >
           {({ handleChange, handleBlur, values, isSubmitting }) => (
-            <>
-              {(showConsultas && (
-                <AgendamentoTable
-                  table1="Nome do Cliente"
-                  table2="Horário Agendado"
-                  table3="Ações"
-                  listAgendamentos={agendamentos}
-                  onSelectAgendamento={onSelectAgendamento}
-                  setAgendamentos={setAgendamentos}
-                  currentPage={currentPage}
-                  setCurrentPage={setCurrentPage}
-                  totalPages={1}
+            <Form className={styles.form}>
+              <div className={styles.formHeader}>
+                <div className={styles.userProfile}>
+                  <div className={styles.photo}>
+                    <p>Imagem de usuário</p>
+                  </div>
+                  <div className={styles.userInfo}>
+                    <h2>{values.name}</h2>
+                    {values.services && values.services.length > 0 ? (
+                      values.services.map((service) => (
+                        <p key={service.id}>{service.name}</p>
+                      ))
+                    ) : (
+                      <p>Nenhum serviço disponível</p>
+                    )}
+                  </div>
+                </div>
+                <div className={styles.agendamento}>
+                  <button
+                    onClick={() => setShowConsultas(!showConsultas)}
+                    className={styles.btn}
+                  >
+                    Atendimentos
+                  </button>
+                </div>
+              </div>
+
+              <h2>Informações pessoais</h2>
+              <div className={styles.personalInfo}>
+                <BarberInput
+                  disabled={!editar}
+                  type="text"
+                  label="Nome completo:"
+                  name="name"
+                  value={values.name ?? ""}
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                  size="medium"
+                  error={undefined}
                 />
-              )) || (
-                <Form className={styles.form}>
-                  <div className={styles.formHeader}>
-                    <div className={styles.userProfile}>
-                      <div className={styles.photo}>
-                        <p>Imagem de usuário</p>
-                      </div>
-                      <div className={styles.userInfo}>
-                        <h2>{values.name}</h2>
-                        {values.services && values.services.length > 0 ? (
-                          values.services.map((service) => (
-                            <p key={service.id}>{service.name}</p>
-                          ))
-                        ) : (
-                          <p>Nenhum serviço disponível</p>
-                        )}
-                      </div>
-                    </div>
-                    <div className={styles.agendamento}>
-                      <button onClick={() => setShowConsultas(true)}>
-                        Atendimentos
-                      </button>
-                    </div>
-                  </div>
+                <BarberInput
+                  disabled={!editar}
+                  type="email"
+                  label="Email:"
+                  name="email"
+                  value={values.email ?? ""}
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                  size="medium"
+                  error={undefined}
+                />
+                <BarberInput
+                  disabled={!editar}
+                  type="text"
+                  label="CPF:"
+                  name="cpf"
+                  value={values.cpf ?? ""}
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                  size="medium"
+                  error={undefined}
+                />
+              </div>
 
-                  <h2>Informações pessoais</h2>
-                  <div className={styles.personalInfo}>
-                    <BarberInput
-                      disabled={!editar}
-                      type="text"
-                      label="Nome completo:"
-                      name="name"
-                      value={values.name || ""}
-                      onChange={handleChange}
-                      onBlur={handleBlur}
-                      size="medium"
-                      error={undefined}
-                    />
-                    <BarberInput
-                      disabled={!editar}
-                      type="email"
-                      label="Email:"
-                      name="email"
-                      value={values.email || ""}
-                      onChange={handleChange}
-                      onBlur={handleBlur}
-                      size="medium"
-                      error={undefined}
-                    />
-                    <BarberInput
-                      disabled={!editar}
-                      type="text"
-                      label="CPF:"
-                      name="cpf"
-                      value={values.cpf || ""}
-                      onChange={handleChange}
-                      onBlur={handleBlur}
-                      size="medium"
-                      error={undefined}
-                    />
-                  </div>
+              <hr className={styles.line} />
+              <h2>Endereço</h2>
+              <div className={styles.address}>
+                <BarberInput
+                  disabled={!editar}
+                  type="text"
+                  label="CEP:"
+                  name="address.cep"
+                  value={address?.cep ?? ""}
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                  size="medium"
+                  error={undefined}
+                />
+                <BarberInput
+                  disabled={!editar}
+                  type="text"
+                  label="Rua:"
+                  name="address.street"
+                  value={address?.street ?? ""}
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                  size="medium"
+                  error={undefined}
+                />
+                <BarberInput
+                  disabled={!editar}
+                  type="number"
+                  label="Número:"
+                  name="address.number"
+                  value={address?.number ?? ""}
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                  size="medium"
+                  error={undefined}
+                />
+                <BarberInput
+                  disabled={!editar}
+                  type="text"
+                  label="Bairro:"
+                  name="address.neighborhood"
+                  value={address?.neighborhood ?? ""}
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                  size="medium"
+                  error={undefined}
+                />
+                <BarberInput
+                  disabled={!editar}
+                  type="text"
+                  label="Cidade:"
+                  name="address.city"
+                  value={address?.city ?? ""}
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                  size="medium"
+                  error={undefined}
+                />
+                <BarberInput
+                  disabled={!editar}
+                  type="text"
+                  label="Estado:"
+                  name="address.state"
+                  value={address?.state ?? ""}
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                  size="medium"
+                  error={undefined}
+                />
+              </div>
 
-                  <hr className={styles.line} />
-                  <h2>Endereço</h2>
-                  <div className={styles.address}>
-                    <BarberInput
-                      disabled={!editar}
-                      type="text"
-                      label="CEP:"
-                      name="address.cep"
-                      value={address?.cep || ""}
-                      onChange={handleChange}
-                      onBlur={handleBlur}
-                      size="medium"
-                      error={undefined}
-                    />
-                    <BarberInput
-                      disabled={!editar}
-                      type="text"
-                      label="Rua:"
-                      name="address.street"
-                      value={address?.street || ""}
-                      onChange={handleChange}
-                      onBlur={handleBlur}
-                      size="medium"
-                      error={undefined}
-                    />
-                    <BarberInput
-                      disabled={!editar}
-                      type="number"
-                      label="Número:"
-                      name="address.number"
-                      value={address?.number || ""}
-                      onChange={handleChange}
-                      onBlur={handleBlur}
-                      size="medium"
-                      error={undefined}
-                    />
-                    {/* <BarberInput
-                    disabled={!editar}
-                    type="text"
-                    label="Complemento:"
-                    name="address.complement"
-                    value={address?. || ""}
-                    onChange={handleChange}
-                    onBlur={handleBlur}
-                    size="medium"
-                    error={undefined}
-                  /> */}
-                    <BarberInput
-                      disabled={!editar}
-                      type="text"
-                      label="Bairro:"
-                      name="address.neighborhood"
-                      value={address?.neighborhood || ""}
-                      onChange={handleChange}
-                      onBlur={handleBlur}
-                      size="medium"
-                      error={undefined}
-                    />
-                    <BarberInput
-                      disabled={!editar}
-                      type="text"
-                      label="Cidade:"
-                      name="address.city"
-                      value={address?.city || ""}
-                      onChange={handleChange}
-                      onBlur={handleBlur}
-                      size="medium"
-                      error={undefined}
-                    />
-                    <BarberInput
-                      disabled={!editar}
-                      type="text"
-                      label="Estado:"
-                      name="address.state"
-                      value={address?.state || ""}
-                      onChange={handleChange}
-                      onBlur={handleBlur}
-                      size="medium"
-                      error={undefined}
-                    />
-                  </div>
+              <hr className={styles.line} />
+              <h2>Informações de Admissão</h2>
+              <div className={styles.admissionInfo}>
+                <BarberInput
+                  disabled={!editar}
+                  type="number"
+                  label="Salário:"
+                  name="salary"
+                  value={values.salary ?? ""}
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                  size="medium"
+                  error={undefined}
+                />
+                <BarberInput
+                  disabled={!editar}
+                  type="date"
+                  label="Data de Admissão:"
+                  name="admissionDate"
+                  value={values.admissionDate ?? ""}
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                  size="medium"
+                  error={undefined}
+                />
+                <BarberInput
+                  disabled={!editar}
+                  type="number"
+                  label="Carga Horária:"
+                  name="workload"
+                  value={values.workload ?? ""}
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                  size="medium"
+                  error={undefined}
+                />
+              </div>
 
-                  <hr className={styles.line} />
-                  <h2>Informações de Admissão</h2>
-                  <div className={styles.admissionInfo}>
-                    <BarberInput
-                      disabled={!editar}
-                      type="number"
-                      label="Salário:"
-                      name="salary"
-                      value={values.salary || ""}
-                      onChange={handleChange}
-                      onBlur={handleBlur}
-                      size="medium"
-                      error={undefined}
-                    />
-                    <BarberInput
-                      disabled={!editar}
-                      type="date"
-                      label="Data de Admissão:"
-                      name="admissionDate"
-                      value={values.admissionDate || ""}
-                      onChange={handleChange}
-                      onBlur={handleBlur}
-                      size="medium"
-                      error={undefined}
-                    />
-                    <BarberInput
-                      disabled={!editar}
-                      type="text"
-                      label="Tipo de Serviço:"
-                      name="services"
-                      value={
-                        values.services
-                          ?.map((service) => service.name)
-                          .join(", ") || ""
-                      }
-                      onChange={handleChange}
-                      onBlur={handleBlur}
-                      size="medium"
-                      error={undefined}
-                    />
-                    <BarberInput
-                      disabled={!editar}
-                      type="number"
-                      label="Carga Horária Semanal:"
-                      name="weeklyHours"
-                      value={values.workload || ""}
-                      onChange={handleChange}
-                      onBlur={handleBlur}
-                      size="medium"
-                      error={undefined}
-                    />
-                  </div>
-
-                  <div className={styles.buttons}>
-                    <button type="button" onClick={() => setEditar(!editar)}>
-                      {editar ? "Cancelar" : "Editar"}
-                    </button>
-                    <button type="submit" disabled={isSubmitting}>
-                      {editar ? "Salvar" : "Editar"}
-                    </button>
-                  </div>
-                </Form>
-              )}
-            </>
+              <div className={styles.buttons}>
+                <button
+                  type="button"
+                  className={styles.btn}
+                  onClick={() => setEditar(!editar)}
+                >
+                  {editar ? "Cancelar" : "Editar"}
+                </button>
+                <button
+                  type="submit"
+                  className={`${styles.btn} ${styles.submitBtn}`}
+                  disabled={isSubmitting || !editar}
+                >
+                  Salvar
+                </button>
+              </div>
+            </Form>
           )}
         </Formik>
+
+        {showConsultas && (
+          <div className={styles.consultas}>
+            <hr className={styles.line}/>
+            <h2>Consultas do Barbeiro</h2>
+            <AgendamentoTable
+              table1="Nome do Cliente"
+              table2="Horário Agendado"
+              table3="Tipo de Serviço"
+              listAgendamentos={agendamentos}
+              onSelectAgendamento={onSelectAgendamento}
+              setAgendamentos={setAgendamentos}
+              totalPages={1}
+              currentPage={currentPage}
+              setCurrentPage={setCurrentPage}
+            />
+          </div>
+        )}
       </div>
     </>
   );
